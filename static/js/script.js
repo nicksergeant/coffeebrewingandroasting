8b824a5d4a60ee1b2bdb5cf95973261819c3e242
Nick Sergeant <nick@nicksergeant.com>
2013-01-11 00:29:58 -0500

    Local LiveReload only, move around some JS, consolidate stylesheets.

---
+++ static/js/script.js
@@ -0,0 +1,15 @@
+var _gauges = _gauges || [];
+(function() {
+    var t   = document.createElement('script');
+    t.type  = 'text/javascript';
+    t.async = true;
+    t.id    = 'gauges-tracker';
+    t.setAttribute('data-site-id', '50ef8e8e108d7b25f5000013');
+    t.src = '//secure.gaug.es/track.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(t, s);
+})();
+
+if (location.host === 'localhost:4000') {
+    document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')
+}
